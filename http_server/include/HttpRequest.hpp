806304f98af192fc2ccf39a37cf480eda8fdab42
Nikita Ermilov <ermilov1999@gmail.com>
2020-11-06 21:18:28 +0300

    HttpMessage interface started

--- http_server/include/HttpRequest.hpp
+++ http_server/include/HttpRequest.hpp
@@ -1,13 +1,25 @@
 #pragma once
 
 #include <vector>
-
+#include <map>
+#include <string>
 
 class HttpMessage {
+ public:
+    virtual std::string Host();
+    std::string User_Agent();
  protected:
+    std::vector<std::string> headers;
+};
+
+class HttpResponse: public HttpMessage {
 
 };
 
 class HttpRequest: public HttpMessage {
+public:
+    HttpRequest(const std::string &message);
+
+
 
 };

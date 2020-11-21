14e8c7deffe6a70496a01135c4f43d702c5c4a4e
KVolkov <volkov.ks@yandex.ru>
2020-11-21 18:55:18 +0300

    playback template (no interface to db / filemanager)

---
+++ ApplicationServer/Libraries/VideoPlayback/src/VideoPlayback.cpp
@@ -0,0 +1,2 @@
+#include "../include/VideoPlayback.hpp"
+
---
+++ ApplicationServer/Libraries/VideoPlayback/test/Test.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+// #include "gmock/gmock.h"
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include "VideoPlayback.hpp"
+
+
+
+TEST(VideoPlayback, initial_test) {
+    
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}

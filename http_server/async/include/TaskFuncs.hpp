6243b4a52413f54b00a03cd8df922ecf208794e9
Mikhail Aglicheev <toba.ru@mail.ru>
2020-12-18 16:08:01 +1100

    Added TaskFuncs

--- http_server/async/include/TaskFuncs.hpp
+++ http_server/async/include/TaskFuncs.hpp
@@ -5,7 +5,7 @@
 #include "HTTPClient.hpp"
 
 // Main function should also set output client (maybe add list of them???).
-typedef std::function<void(std::vector<char>&, HTTPClient&)> MainFuncType;
-MainFuncType PreProcess(std::vector<char>& data, HTTPClient& input);
-void MainProcessBasic(std::vector<char>& data, HTTPClient& output);
-void PostProcess(std::vector<char>& data, HTTPClient& output);
+typedef std::function<void(std::string&, std::vector<char>&, HTTPClient&)> MainFuncType;
+MainFuncType PreProcess(std::string& headers, std::vector<char>& data, HTTPClient& input);
+void MainProcessBasic(std::string& headers, std::vector<char>& body, HTTPClient& output);
+void PostProcess(std::string& headers, std::vector<char>& body, HTTPClient& output);

13b30bca7bd42f31051c88215412687de541481b
Mikhail Aglicheev <toba.ru@mail.ru>
2020-12-24 19:24:50 +1100

    Added basic database mock, it works

--- http_server/async/src/TaskFuncs.cpp
+++ http_server/async/src/TaskFuncs.cpp
@@ -60,7 +60,7 @@ void MainProcessBasic(std::map<std::string, std::string>& headers, std::vector<c
             body.clear();
             std::string sdString = std::to_string(input.getSd());
             body.insert(body.end(), sdString.begin(), sdString.end());
-            std::vector<char> paramsPart = HTTPClient::mergeSetToVector(params);
+            std::vector<char> paramsPart = std::move(HTTPClient::mergeSetToVector(params));
             body.insert(body.end(), paramsPart.begin(), paramsPart.end());
 
             output = HTTPClient("localhost", 7777);
@@ -83,66 +83,63 @@ void MainProcessDBServer(std::map<std::string, std::string>& headers, std::vecto
                          std::shared_ptr<std::mutex> pendingDBResponseMutex,
                          HTTPClient& input, HTTPClient& output) {
     auto firstSepPos = std::find(body.begin(), body.end(), '|');
-    TemplateManager templateManager(headers["url"]);
-
-    body.erase(firstSepPos + 1, body.end());  // erasing everything after sd
-    // Replace this with actual body parameter handling
-    body.push_back('|');
-
-    std::ifstream source("../static/index.html", std::ios::binary);
-    char buffer[BUF_SIZE] = {0};
-    while (source.read(buffer, BUF_SIZE)) {
-        body.insert(body.end(), buffer, buffer + BUF_SIZE);
+    if (firstSepPos > body.end()) {
+        firstSepPos = body.end();
     }
-    body.insert(body.end(), buffer, buffer + source.gcount());
-
-    output = HTTPClient("localhost", 6666);
+    TemplateManager templateManager(headers["url"]);
 
     // vector for randomizing movies (before actual recommendations, based on movie watching experience
     std::vector<std::string> vect;
-        vect.push_back("images/img6.jpg");
-        vect.push_back("images/img5.jpg");
-        vect.push_back("images/img4.jpg");
-        vect.push_back("images/img3.jpg");
-        vect.push_back("images/img2.jpg");
-        vect.push_back("images/img1.jpg");
-        unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-        std::shuffle(std::begin(vect), std::end(vect), std::default_random_engine(seed));
+    vect.push_back("images/img6.jpg");
+    vect.push_back("images/img5.jpg");
+    vect.push_back("images/img4.jpg");
+    vect.push_back("images/img3.jpg");
+    vect.push_back("images/img2.jpg");
+    vect.push_back("images/img1.jpg");
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::shuffle(std::begin(vect), std::end(vect), std::default_random_engine(seed));
 
     // map of actual parameters
-        std::map<std::string, std::string> params; 
-        params["movietittle"] = "Titanic";
-        params["moviedescription"] = "Subscribe Woosh.com to watch more kittens.";
-        params["starphoto"] = "images/Leo.jpeg";
-        params["starname"] = "Leonardo Dicaprio";
-        params["movielogo"] = "images/img1.jpg";
-        params["moviename"] = "Titanic";
-        params["videolink"] = "lorem_ipsum.mp4";
-        params["movierating"] = "3";
-        params["recommended"] = std::to_string(vect.size());
+    std::map<std::string, std::string> params; 
+    params["movietittle"] = "Titanic";
+    params["moviedescription"] = "Subscribe Woosh.com to watch more kittens.";
+    params["starphoto"] = "images/Leo.jpeg";
+    params["starname"] = "Leonardo Dicaprio";
+    params["movielogo"] = "images/img1.jpg";
+    params["moviename"] = "Titanic";
+    params["videolink"] = "lorem_ipsum.mp4";
+    params["movierating"] = "3";
+    params["recommended"] = std::to_string(vect.size());
     // this cycle adds shuffled parameters, where vector above you can add multiple linked paramets, number has to be same
     // i.e. vector<string> for href and tittle of recommended movies 
     for (size_t i = 0; i < vect.size(); i++)
-            params["recommended" + std::to_string(i)] = vect[i];
+        params["recommended" + std::to_string(i)] = vect[i];
+    
+    params["sd"] = std::string(body.begin(), firstSepPos);
+
+    body = std::move(HTTPClient::mergeMapToVector(params));
+
+    output = HTTPClient("localhost", 6666);
 }
+
 void MainProcessDBReceived(std::map<std::string, std::string>& headers, std::vector<char>& body,
                            std::map<int, HTTPClient>& pendingDBResponse,
                            std::shared_ptr<std::mutex> pendingDBResponseMutex,
                            HTTPClient& input, HTTPClient& output) {
-    std::queue<std::string> bodyParams = HTTPClient::splitVectorToQueue(body);
-    int sd = std::stoi(bodyParams.front());
-    bodyParams.pop();
+    std::map<std::string, std::string> bodyParams = std::move(HTTPClient::splitVectorToMap(body));
+
+    int sd = std::stoi(bodyParams.at("sd"));
+    bodyParams.erase("sd");
+
+    TemplateManager templateManager(headers["url"]);
+    body = std::move(templateManager.GetHtmlFinal(bodyParams));
+
     output = pendingDBResponse.at(sd);  //  thread-safe
     pendingDBResponseMutex->lock();
     pendingDBResponse.erase(sd);
     pendingDBResponseMutex->unlock();
-    // Template postprocesing goes here
-    // headers are taken from pending client as if no DB accessing happened
-    // body is resulting document
-
-    // delete this later
-    body = HTTPClient::mergeQueueToVector(bodyParams);
 }
+
 void PostProcess(std::map<std::string, std::string>& headers, std::vector<char>& body, HTTPClient& output) {
     HttpResponse response(headers["http_version"],
                           HttpRequest::StringToRequestMethod(headers["method"]),
